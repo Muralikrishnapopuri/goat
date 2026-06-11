5d574e8628a475f27ea886cc96a0dc528009b89e
Micheal <popurimuralikrishna04@gmail.com>
2026-06-11 17:12:37 +0530

    new

--- demo-practice.js
+++ demo-practice.js
@@ -1,12 +1,37 @@
-function person(name){
+function person(name) {
     this.name = name;
-    this.great= function(){
-        console.log("im"+this.name);
+    this.great = function () {
+        console.log("im" + this.name);
     }
 }
 
 const per1 = new person("alice");
-const per2 =new person("krish");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
 
 per1.great();
 per2.great();
