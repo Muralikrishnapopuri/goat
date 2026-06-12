157d94f4e3d580495fc71ba247bcccc36f70e48f
Micheal <popurimuralikrishna04@gmail.com>
2026-06-12 18:07:16 +0530

    new chnages

--- demo-practice.js
+++ demo-practice.js
@@ -35,3 +35,338 @@ const per2 = new person("krish");
 
 per1.great();
 per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
+function person(name) {
+    this.name = name;
+    this.great = function () {
+        console.log("im" + this.name);
+    }
+}
+
+const per1 = new person("alice");
+const per2 = new person("krish");
+
+per1.great();
+per2.great();
