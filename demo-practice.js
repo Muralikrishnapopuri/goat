1b606a85b49ff6728a6279b96a8697c97426a1f6
Micheal <popurimuralikrishna04@gmail.com>
2026-06-30 16:38:41 +0530

    new

--- demo-practice.js
+++ demo-practice.js
@@ -17,39 +17,3 @@ console.log(calcualatecashe(2));
 console.log(calcualatecashe(2));
 
 
-
-function logic(fn){
-    const cache = new Map();
-    return function innerlogic(...args){
-        const key = JSON.stringify(args);
-        if(cache.has(key)) return cache.get(key);
-
-        const result = fn(...args);
-         cache.set(key,result);
-         return result;
-    }
-}
-const calcualatecashe = logic((n)=>{
-    console.log("computing,,")
-    return n*n;
-})
-console.log(calcualatecashe(2));
-console.log(calcualatecashe(2));
-
-function logic(fn){
-    const cache = new Map();
-    return function innerlogic(...args){
-        const key = JSON.stringify(args);
-        if(cache.has(key)) return cache.get(key);
-
-        const result = fn(...args);
-         cache.set(key,result);
-         return result;
-    }
-}
-const calcualatecashe = logic((n)=>{
-    console.log("computing,,")
-    return n*n;
-})
-console.log(calcualatecashe(2));
-console.log(calcualatecashe(2));
