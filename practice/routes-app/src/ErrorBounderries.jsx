985ae0a54347c6c68997709dfd290293ff260741
Micheal <popurimuralikrishna04@gmail.com>
2026-07-10 13:30:32 +0530

    dublicate commit

--- practice/routes-app/src/ErrorBounderries.jsx
+++ practice/routes-app/src/ErrorBounderries.jsx
@@ -27,3 +27,33 @@ class ErrorBounderries extends React.Component {
   }
 }  
 
+
+class ErrorBounderries extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: true,
+    };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error);
+    console.log(errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something Went Wrong.</h2>;
+    }
+
+    return this.props.children;
+  }
+}  
+
