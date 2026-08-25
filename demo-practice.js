b3e133bdfba2e8d1482cc3a676aab7d53e3d2423
Micheal <popurimuralikrishna04@gmail.com>
2026-08-25 15:05:41 +0530

    latest updated

--- demo-practice.js
+++ demo-practice.js
@@ -1,1 +1,38 @@
-const 
+// const curry = (fn) => {
+//     return function curried(...args) {
+//         if (args.length >= fn.length) {
+//              return fn(...args);
+//         }
+//         return (...newargs) => curried(...args, ...newargs);
+//     }
+// }
+// const add = curry((i, j, k) => i + j + k);
+// console.log(add(1,12)(44));
+
+// const memorize= (fn)=>{
+//     const cache = new Map();
+//     return function (...args){
+//         const key = JSON.stringify(args);
+//         if(cache.has(key)) return cache.get(key);
+//         const res = fn(...args);
+//         cache.set(key,res);
+//         return res;
+//     }
+// }
+
+// const expcal = memorize((input)=>{
+//     console.log("..trigger here");
+//     return input*input
+// }); 
+
+ 
+
+
+// console.log(expcal(4));
+
+
+console.log([] == false);    
+console.log([] == ![]);       
+console.log({} == !{}); 
+
+
